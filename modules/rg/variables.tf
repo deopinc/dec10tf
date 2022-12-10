b30649629a544fa4327665724df78a66197c402c
Erol Kavas <erol@Erols-MBP.ht.home>
2022-12-10 12:44:38 -0500

    add new files

---
+++ modules/rg/variables.tf
@@ -0,0 +1,18 @@
+variable "location" {
+  default = "West US"
+}
+
+variable "rg_name" {
+  default  = "data"
+  type = string
+}
+
+variable "env" {
+    default = "dev"
+  type = string
+}
+
+variable "costcenter" {
+    default = "0"
+  type = any
+}
